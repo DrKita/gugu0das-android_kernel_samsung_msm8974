473800a55c2df2bc7a7863866db03f46428bcd36
gugu0das <gugu0das@gmail.com>
2017-01-21 02:00:33 +0900

    build : Kernel Flashing File Creation
    Conflicts:
    	build_kernel-jactivelteskt.sh

--- cleanup.sh
+++ cleanup.sh
@@ -3,3 +3,17 @@
 # Clean Build Files
 make mrproper
 rm -r -f output/
+rm -f gugu0das/Cyanogenmod_13.0/ramdisk.cpio.gz
+rm -f gugu0das/Cyanogenmod_13.0/boot_hlteskt.img
+rm -f gugu0das/Cyanogenmod_13.0/boot_hlteeur.img
+rm -f gugu0das/Touchwiz/ramdisk.cpio.gz
+rm -f gugu0das/Touchwiz/boot_hlteskt.img
+rm -f gugu0das/Touchwiz/boot_hlteeur.img
+rm -f gugu0das/gugu0das_kernel-NX_CM-13.0-hlteskt-Release-1/boot.img
+rm -f gugu0das/gugu0das_kernel-NX_TW-M-hlteskt-Release-1/boot.img
+rm -f gugu0das_kernel-NX_CM-13.0-hlteskt-Release-1.zip
+rm -f gugu0das_kernel-NX_TW-M-hlteskt-Release-1.zip
+rm -f gugu0das/gugu0das_kernel-NX_CM-13.0-hlteeur-Release-1/boot.img
+rm -f gugu0das/gugu0das_kernel-NX_TW-M-hlteeur-Release-1/boot.img
+rm -f gugu0das_kernel-NX_CM-13.0-hlteeur-Release-1.zip
+rm -f gugu0das_kernel-NX_TW-M-hlteeur-Release-1.zip

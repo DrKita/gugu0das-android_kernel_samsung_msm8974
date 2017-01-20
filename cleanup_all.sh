e66f05e60fc7a2ccedd97f149aae2980448facd9
gugu0das <gugu0das@gmail.com>
2017-01-21 02:00:33 +0900

    build : Kernel Flashing File Creation
    Conflicts:
    	build_kernel-jactivelteskt.sh

--- cleanup_all.sh
+++ cleanup_all.sh
@@ -5,3 +5,8 @@ make mrproper
 rm -r -f output/
 rm -r -f armeb-linux-gnueabihf-linaro_6.2.1-2016.11/
 rm -r -f arm-eabi-ubertc_7.0-2016.04/
+rm -f gugu0das/Cyanogenmod_13.0/ramdisk.cpio.gz
+rm -f gugu0das/Cyanogenmod_13.0/boot.img
+rm -f gugu0das/Cyanogenmod_13.0/kernel
+rm -f gugu0das/gugu0das_kernel-NX_CM-13.0-ks01lteskt-Release-1/boot.img
+rm -f gugu0das_kernel-NX_CM-13.0-ks01lteskt-Release-1.zip

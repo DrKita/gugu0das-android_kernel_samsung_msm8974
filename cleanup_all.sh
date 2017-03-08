909fcc0cd6392cefeeffd2b49bf147ea5ed730d4
gugu0das <gugu0das@gmail.com>
2017-03-08 23:09:14 +0900

    Release-2-BetaProgram_2017.03.08

--- cleanup_all.sh
+++ cleanup_all.sh
@@ -10,7 +10,7 @@ rm -f gugu0das/Cyanogenmod_14.1/boot_hltekor.img
 rm -f gugu0das/Cyanogenmod_14.1/kernel_hltekor
 rm -f gugu0das/Cyanogenmod_14.1/boot_hlteeur.img
 rm -f gugu0das/Cyanogenmod_14.1/kernel_hlteeur
-rm -f gugu0das/gugu0das_kernel-NX_CM-14.1-hltekor-Release-1/boot.img
-rm -f gugu0das_kernel-NX_CM-14.1-hltekor-Release-1.zip
-rm -f gugu0das/gugu0das_kernel-NX_CM-14.1-hlteeur-Release-1/boot.img
-rm -f gugu0das_kernel-NX_CM-14.1-hlteeur-Release-1.zip
+rm -f gugu0das/gugu0das_kernel-NX_CM-14.1-hltekor-Release-2-BetaProgram_2017.03.08/boot.img
+rm -f gugu0das_kernel-NX_CM-14.1-hltekor-Release-2-BetaProgram_2017.03.08.zip
+rm -f gugu0das/gugu0das_kernel-NX_CM-14.1-hlteeur-Release-2-BetaProgram_2017.03.08/boot.img
+rm -f gugu0das_kernel-NX_CM-14.1-hlteeur-Release-2-BetaProgram_2017.03.08.zip

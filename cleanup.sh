e6db4f8588446a84601aeb91504aa94616d99d01
gugu0das <gugu0das@gmail.com>
2017-03-24 14:08:26 +0900

    Release-2-BetaProgram_2017.03.24

--- cleanup.sh
+++ cleanup.sh
@@ -6,7 +6,7 @@ rm -r -f output/
 rm -f gugu0das/Touchwiz/ramdisk.cpio.gz
 rm -f gugu0das/Touchwiz/boot_kltekor.img
 rm -f gugu0das/Touchwiz/boot_klteeur.img
-rm -f gugu0das/gugu0das_kernel-NX_TW-M-kltekor-Release-2-BetaProgram_2017.03.08/boot.img
-rm -f gugu0das_kernel-NX_TW-M-kltekor-Release-2-BetaProgram_2017.03.08.zip
-rm -f gugu0das/gugu0das_kernel-NX_TW-M-klteeur-Release-2-BetaProgram_2017.03.08/boot.img
-rm -f gugu0das_kernel-NX_TW-M-klteeur-Release-2-BetaProgram_2017.03.08.zip
+rm -f gugu0das/gugu0das_kernel-NX_TW-M-kltekor-Release-2-BetaProgram_2017.03.24/boot.img
+rm -f gugu0das_kernel-NX_TW-M-kltekor-Release-2-BetaProgram_2017.03.24.zip
+rm -f gugu0das/gugu0das_kernel-NX_TW-M-klteeur-Release-2-BetaProgram_2017.03.24/boot.img
+rm -f gugu0das_kernel-NX_TW-M-klteeur-Release-2-BetaProgram_2017.03.24.zip

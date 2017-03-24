3d995f4a1923b125df66c325837f6e2fd1740aa1
gugu0das <gugu0das@gmail.com>
2017-03-24 14:05:20 +0900

    Release-2-BetaProgram_2017.03.24

--- cleanup_all.sh
+++ cleanup_all.sh
@@ -8,5 +8,5 @@ rm -r -f arm-eabi-ubertc_7.0-2016.04/
 rm -f gugu0das/Cyanogenmod_14.1/ramdisk.cpio.gz
 rm -f gugu0das/Cyanogenmod_14.1/boot.img
 rm -f gugu0das/Cyanogenmod_14.1/kernel
-rm -f gugu0das/gugu0das_kernel-NX_CM-14.1-jactivelteskt-Release-2-BetaProgram_2017.03.18/boot.img
-rm -f gugu0das_kernel-NX_CM-14.1-jactivelteskt-Release-2-BetaProgram_2017.03.18.zip
+rm -f gugu0das/gugu0das_kernel-NX_CM-14.1-jactivelteskt-Release-2-BetaProgram_2017.03.24/boot.img
+rm -f gugu0das_kernel-NX_CM-14.1-jactivelteskt-Release-2-BetaProgram_2017.03.24.zip

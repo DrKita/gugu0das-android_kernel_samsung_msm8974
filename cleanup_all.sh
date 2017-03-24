8d8b70fe875bd48e40034cf3bdbb9b11ef9a2ba6
gugu0das <gugu0das@gmail.com>
2017-03-24 14:07:27 +0900

    Release-2-BetaProgram_2017.03.24

--- cleanup_all.sh
+++ cleanup_all.sh
@@ -8,5 +8,5 @@ rm -r -f arm-eabi-ubertc_7.0-2016.04/
 rm -f gugu0das/Cyanogenmod_13.0/ramdisk.cpio.gz
 rm -f gugu0das/Cyanogenmod_13.0/boot.img
 rm -f gugu0das/Cyanogenmod_13.0/kernel
-rm -f gugu0das/gugu0das_kernel-NX_CM-13.0-ks01ltekor-Release-2-BetaProgram_2017.03.18/boot.img
-rm -f gugu0das_kernel-NX_CM-13.0-ks01ltekor-Release-2-BetaProgram_2017.03.18.zip
+rm -f gugu0das/gugu0das_kernel-NX_CM-13.0-ks01ltekor-Release-2-BetaProgram_2017.03.24/boot.img
+rm -f gugu0das_kernel-NX_CM-13.0-ks01ltekor-Release-2-BetaProgram_2017.03.24.zip

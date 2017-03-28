e833b9a15d21a6ecbbccce521759bb1bb311c86f
gugu0das <gugu0das@gmail.com>
2017-03-28 20:54:14 +0900

    Release-2

--- cleanup.sh
+++ cleanup.sh
@@ -5,5 +5,5 @@ make mrproper
 rm -r -f output/
 rm -f gugu0das/Cyanogenmod_13.0/ramdisk.cpio.gz
 rm -f gugu0das/Cyanogenmod_13.0/boot.img
-rm -f gugu0das/gugu0das_kernel-NX_CM-13.0-ks01ltekor-Release-2-BetaProgram_2017.03.24/boot.img
-rm -f gugu0das_kernel-NX_CM-13.0-ks01ltekor-Release-2-BetaProgram_2017.03.24.zip
+rm -f gugu0das/gugu0das_kernel-CM-13.0-ks01ltekor-Release-2/boot.img
+rm -f gugu0das_kernel-CM-13.0-ks01ltekor-Release-2.zip
